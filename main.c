09722ca753e147b201becb141f55df7340387a2b
Dom <55925313+LinnyMimatsu@users.noreply.github.com>
2025-11-18 14:06:14 -0600

    Game resets if the user takes too long to make an input

--- main.c
+++ main.c
@@ -6,6 +6,7 @@ Description: Pocket Simon using theory learned in class for the MSP-EXP430G2ET m
 #include <msp430.h>
 #include <stdlib.h>
 
+volatile unsigned char timer_counter = 0;
 volatile unsigned char last_button = 0;
 
 /*Hard coded length of the game, can be reduced or lengthened*/
@@ -14,7 +15,7 @@ unsigned char sequence[MAX_SEQUENCE];
 unsigned char sequence_length = 0;
 unsigned char player_step = 0;
 
-
+#define DELAY_BEFORE_THE_PLAYER_FAILS_FOR_NOT_PRESSING_A_BUTTON_MEASURED_IN_SECONDS 5
 
 int main(void)
 {
@@ -27,8 +28,7 @@ int main(void)
     P1DIR |= (BIT0 | BIT6);
     P1OUT &= ~(BIT0 | BIT6);
 
-    add_random_step();
-    play_sequence();
+
 
     /*Setting up the buttons for the simon game*/
     P1DIR &= ~(BIT1 | BIT2 | BIT4 | BIT5);
@@ -39,19 +39,26 @@ int main(void)
     TA0CTL = TASSEL_2 | MC_2 | TACLR;
     srand(TA0R);
 
+    TA1CCR0 = 12500;        
+    TA1CCTL0 = CCIE;            
+    TA1CTL = TASSEL_2 | ID_3; 
+
     /*Enabling the interrupts for the buttons*/
     P1IE |= (BIT1 | BIT2 | BIT4 | BIT5);
     P1IES |= (BIT1 | BIT2 | BIT4 | BIT5);
     P1IFG &= ~(BIT1 | BIT2 | BIT4 | BIT5);
 
-
     __bis_SR_register(GIE);
+
+    add_random_step();
+    play_sequence();
 
     while(1)
     {
       
       check_player_input();
 
+   
   }
 }
 
@@ -89,10 +96,33 @@ __interrupt void P1_ISR(void) {
 
 }
 
+/*using a timer, reset the game if the user takes too long*/
+#pragma vector=TIMER1_A0_VECTOR
+__interrupt void Timer1_A0_ISR(void) {
+
+  if(++timer_counter == (10 * DELAY_BEFORE_THE_PLAYER_FAILS_FOR_NOT_PRESSING_A_BUTTON_MEASURED_IN_SECONDS)){
+    TA1CTL &= ~MC0;
+    TA1CTL |= TACLR;
+    timer_counter = 0;
+
+    sequence_length = 0;
+    player_step = 0;
+    flash_board_led(BIT6);
+    add_random_step();
+    play_sequence();
+
+
+  }
+}
+
 /*checks inputs to leds*/
 void check_player_input() {
 
   if(last_button == 0) return;
+  
+  TA1CTL &= ~MC0;
+  TA1CTL |= TACLR;
+  timer_counter = 0;
 
   unsigned char pressed;
   switch(last_button) {
@@ -119,18 +149,27 @@ void check_player_input() {
   // if user input equals sequence continue
   if(pressed == sequence[player_step]) {
     player_step++;
-  
+
 
   if(player_step == sequence_length) {
     flash_board_led(BIT0);
     player_step = 0;
     add_random_step();
     play_sequence();
+  }else{
+
+  TA1CTL |= MC0;
+  
   }
+
+  
 
 
   // if user messes up restart
   } else {
+      TA1CTL &= ~MC0;
+      TA1CTL |= TACLR;
+      timer_counter = 0;
 
     sequence_length = 0;
     flash_board_led(BIT6);
@@ -152,6 +191,28 @@ if(sequence_length < MAX_SEQUENCE) {
   sequence[sequence_length] = rand() % 4;
   sequence_length++;
 
+
+/*Celebratory flashing and reset the sequence*/
+}else{
+
+      TA1CTL &= ~MC0;
+      TA1CTL |= TACLR;
+      timer_counter = 0;
+      unsigned char i;
+      for (i = 0; i <= 5; i++){
+      P2OUT |= (BIT0 | BIT1 | BIT2 | BIT3);
+      
+      __delay_cycles(300000);
+
+      P2OUT &= ~(BIT0 | BIT1 | BIT2 | BIT3);
+
+      __delay_cycles(200000);
+      }
+
+      sequence_length = 0;
+      player_step = 0;
+      add_random_step();
+    
 }
 
 }
@@ -163,7 +224,12 @@ void play_sequence() {
     flash_led(sequence[i]);
   }
 
+  TA1CTL |= TACLR;
+  timer_counter = 0;
+  TA1CTL |= MC0;
+
 }
+
 
 /*function that actually does the flashing*/
 void flash_led(unsigned char led) {
@@ -198,6 +264,7 @@ __delay_cycles(100000);
 
 
 }
+
 
 /*if the user gets the sequence right flash green, if not
 flash red on the board*/
